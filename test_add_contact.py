2716a66731e6a056e68c046fda3f863858dc4698
mihaosin <mikhail.osinovskiy@mail.ru>
2022-06-23 21:30:24 +0300

    создали вспомогательные методы, соответствующие действиям при создании нового контакта

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,77 +1,59 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r'')
-        self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
+        self.wd = webdriver.Chrome()
+        self.wd.implicitly_wait(30)
 
     def test_add_contact(self):
-        driver = self.driver
-        driver.get("http://localhost/addressbook/")
-        driver.find_element_by_name("user").click()
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
-        driver.find_element_by_xpath("//input[@value='Login']").click()
-        driver.find_element_by_link_text("add new").click()
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("Vasia")
-        driver.find_element_by_name("lastname").click()
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Petrov")
-        driver.find_element_by_name("address").click()
-        driver.find_element_by_name("address").clear()
-        driver.find_element_by_name("address").send_keys("Texas")
-        driver.find_element_by_name("email").click()
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("perov@adress.book")
-        driver.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        driver.find_element_by_link_text("home").click()
-        driver.find_element_by_link_text("Logout").click()
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.create_contact(wd)
+        # переход на домашнюю страницу
+        wd.find_element(by=By.LINK_TEXT, value="home").click()
+        # выход из учетной записи
+        wd.find_element(by=By.LINK_TEXT, value="Logout").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
+    def create_contact(self, wd):
+        # начало создания нового контакта
+        wd.find_element(by=By.LINK_TEXT, value="add new").click()
+        # заполнение свойств котакта
+        wd.find_element(by=By.NAME, value="firstname").click()
+        wd.find_element(by=By.NAME, value="firstname").clear()
+        wd.find_element(by=By.NAME, value="firstname").send_keys("Vasia")
+        wd.find_element(by=By.NAME, value="lastname").click()
+        wd.find_element(by=By.NAME, value="lastname").clear()
+        wd.find_element(by=By.NAME, value="lastname").send_keys("Petrov")
+        wd.find_element(by=By.NAME, value="address").click()
+        wd.find_element(by=By.NAME, value="address").clear()
+        wd.find_element(by=By.NAME, value="address").send_keys("Texas")
+        wd.find_element(by=By.NAME, value="email").click()
+        wd.find_element(by=By.NAME, value="email").clear()
+        wd.find_element(by=By.NAME, value="email").send_keys("perov@adress.book")
+        # завершение создания котакта
+        wd.find_element(by=By.NAME, value="submit").click()
 
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
+    def login(self, wd, username, password):
+        # авторизация в учетной записи
+        wd.find_element(by=By.NAME, value="user").click()
+        wd.find_element(by=By.NAME, value="user").clear()
+        wd.find_element(by=By.NAME, value="user").send_keys(username)
+        wd.find_element(by=By.NAME, value="pass").click()
+        wd.find_element(by=By.NAME, value="pass").clear()
+        wd.find_element(by=By.NAME, value="pass").send_keys(password)
+        wd.find_element(by=By.XPATH, value="//input[@value='Login']").click()
 
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
+    def open_home_page(self, wd):
+        # открытие домашней страницы
+        wd.get("http://localhost/addressbook/")
 
     def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+        self.wd.quit()
 
 
 if __name__ == "__main__":
