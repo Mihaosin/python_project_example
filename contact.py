752e60463292a9ffb499729d739cbcd6540c6caf
mihaosin <mikhail.osinovskiy@mail.ru>
2022-06-23 22:23:24 +0300

    создали вспомогательный класс Contact и добавили файл в коммит

---
+++ contact.py
@@ -0,0 +1,7 @@
+
+class Contact:
+    def __init__(self, firstname, lastname, address, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.email = email
