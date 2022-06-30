8803441c3e146a13a168feb0a232a442cad3a612
mihaosin <mikhail.osinovskiy@mail.ru>
2022-06-30 20:18:09 +0300

    добавлен тест модификации группы

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-#from selenium import Alert
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
--- fixture/group.py
+++ fixture/group.py
@@ -8,11 +8,6 @@ class GroupHelper:
         # и сохраняет ее в свойство с именем app
         self.app = app
 
-    def return_to_groups_page(self):
-        wd = self.app.wd
-        # возврат на страницу "Группы"
-        wd.find_element(by=By.LINK_TEXT, value="groups").click()
-
     def create(self, group):
         wd = self.app.wd
         # начало создания новой группы
@@ -32,6 +27,26 @@ class GroupHelper:
         wd.find_element(by=By.NAME, value="submit").click()
         self.return_to_groups_page()
 
+    def edit_first(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # выбрать первую группу (чек-бокс)
+        wd.find_element(by=By.NAME, value="selected[]").click()
+        wd.find_element(by=By.NAME, value="edit").click()
+        # изменить содержимое полей - name, header, footer
+        wd.find_element(by=By.NAME, value="group_name").click()
+        wd.find_element(by=By.NAME, value="group_name").clear()
+        wd.find_element(by=By.NAME, value="group_name").send_keys(group.name)
+        wd.find_element(by=By.NAME, value="group_header").click()
+        wd.find_element(by=By.NAME, value="group_header").clear()
+        wd.find_element(by=By.NAME, value="group_header").send_keys(group.header)
+        wd.find_element(by=By.NAME, value="group_footer").click()
+        wd.find_element(by=By.NAME, value="group_footer").clear()
+        wd.find_element(by=By.NAME, value="group_footer").send_keys(group.footer)
+        # нажать на кнопку подтвердить изменения
+        wd.find_element(by=By.NAME, value="update").click()
+        self.return_to_groups_page()
+
     def delete_first(self):
         wd = self.app.wd
         self.open_groups_page()
@@ -44,4 +59,9 @@ class GroupHelper:
     def open_groups_page(self):
         wd = self.app.wd
         # переход на страницу "Группы"
+        wd.find_element(by=By.LINK_TEXT, value="groups").click()
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        # возврат на страницу "Группы"
         wd.find_element(by=By.LINK_TEXT, value="groups").click()
