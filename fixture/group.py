c88f5d0f345ca7812e56d4e228198491132a6054
mihaosin <mikhail.osinovskiy@mail.ru>
2022-07-05 21:58:25 +0300

    обновлен тест модификации первой группы: выделены вспомогательные методы - выбор первой группы, изменение значения заданного поля; разделили тест на 3 теста - изменение имени, изменение хедера и изменение футера

--- fixture/group.py
+++ fixture/group.py
@@ -13,36 +13,31 @@ class GroupHelper:
         # начало создания новой группы
         self.open_groups_page()
         wd.find_element(by=By.NAME, value="new").click()
-        # заполнение атрибутов группы
-        wd.find_element(by=By.NAME, value="group_name").click()
-        wd.find_element(by=By.NAME, value="group_name").clear()
-        wd.find_element(by=By.NAME, value="group_name").send_keys(group.name)
-        wd.find_element(by=By.NAME, value="group_header").click()
-        wd.find_element(by=By.NAME, value="group_header").clear()
-        wd.find_element(by=By.NAME, value="group_header").send_keys(group.header)
-        wd.find_element(by=By.NAME, value="group_footer").click()
-        wd.find_element(by=By.NAME, value="group_footer").clear()
-        wd.find_element(by=By.NAME, value="group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # завершение создания группы
         wd.find_element(by=By.NAME, value="submit").click()
         self.return_to_groups_page()
 
+    def fill_group_form(self, group):
+        # заполнение атрибутов группы
+        wd = self.app.wd
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element(by=By.NAME, value=field_name).click()
+            wd.find_element(by=By.NAME, value=field_name).clear()
+            wd.find_element(by=By.NAME, value=field_name).send_keys(text)
+
     def edit_first(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        # выбрать первую группу (чек-бокс)
-        wd.find_element(by=By.NAME, value="selected[]").click()
+        self.select_first_group()
         wd.find_element(by=By.NAME, value="edit").click()
-        # изменить содержимое полей - name, header, footer
-        wd.find_element(by=By.NAME, value="group_name").click()
-        wd.find_element(by=By.NAME, value="group_name").clear()
-        wd.find_element(by=By.NAME, value="group_name").send_keys(group.name)
-        wd.find_element(by=By.NAME, value="group_header").click()
-        wd.find_element(by=By.NAME, value="group_header").clear()
-        wd.find_element(by=By.NAME, value="group_header").send_keys(group.header)
-        wd.find_element(by=By.NAME, value="group_footer").click()
-        wd.find_element(by=By.NAME, value="group_footer").clear()
-        wd.find_element(by=By.NAME, value="group_footer").send_keys(group.footer)
+        self.fill_group_form(group)
         # нажать на кнопку подтвердить изменения
         wd.find_element(by=By.NAME, value="update").click()
         self.return_to_groups_page()
@@ -50,11 +45,15 @@ class GroupHelper:
     def delete_first(self):
         wd = self.app.wd
         self.open_groups_page()
-        # выбрать первую группу (чек-бокс)
-        wd.find_element(by=By.NAME, value="selected[]").click()
+        self.select_first_group()
         # нажать на кнопуку "удалить группу"
         wd.find_element(by=By.NAME, value="delete").click()
         self.return_to_groups_page()
+
+    # выбрать первую группу (чек-бокс)
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element(by=By.NAME, value="selected[]").click()
 
     def open_groups_page(self):
         wd = self.app.wd
