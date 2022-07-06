ed25951d8d141064336d1da2be7539fd4036641d
mihaosin <mikhail.osinovskiy@mail.ru>
2022-07-06 21:13:54 +0300

    вызов login и logout перенесли из тестов в инициализацию фикстуры

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -6,12 +6,8 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Vasia", lastname="Petrov", address="Texas", email="perov@adress.book"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", lastname="", address="", email=""))
-    app.session.logout()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,16 +1,12 @@
 from model.group import Group
 
 def test_edit_first_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first(Group(name="new_name"))
-    app.session.logout()
+
 
 def test_edit_first_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first(Group(header="new_header"))
-    app.session.logout()
+
 
 def test_edit_first_group_footer(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first(Group(footer="new_footer"))
-    app.session.logout()
