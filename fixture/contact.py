a4300aa1125db99c63145f73cd26dcfcfdb0d37f
mihaosin <mikhail.osinovskiy@mail.ru>
2022-07-27 21:24:54 +0300

    добавлен тест сравнения котакта со страницы редактирования и со страница просмотра. Метод get_contact_info_from_edit_page работает без ошибок ТОЛЬКОБ ЕСЛИ ЗАПОЛНЕНЫ ВСЕ 3 ПОЛЯ НОМЕРА ТЕЛЕФОНОВ

--- fixture/contact.py
+++ fixture/contact.py
@@ -105,8 +105,48 @@ class ContactHelper:
             self.contact_cache = []
             for row in wd.find_elements(by=By.NAME, value="entry"):
                 cells = row.find_elements(by=By.TAG_NAME, value="td")
-                id = cells[0].find_element(by=By.NAME, value="selected[]").get_attribute("value")
-                lastname = cells[1].text
-                firstname = cells[2].text
-                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id))
+                id = cells[index_id].find_element(by=By.NAME, value="selected[]").get_attribute("value")
+                lastname = cells[index_lastname].text
+                firstname = cells[index_firstname].text
+                all_phones = cells[index_phones].text.splitlines()
+                self.contact_cache.append(Contact(firstname=firstname, lastname=lastname, id=id,
+                                                  homephone=all_phones[0], mobilephone=all_phones[1],
+                                                  workphone=all_phones[2]))
         return list(self.contact_cache)
+
+    def open_contact_to_edit_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements(by=By.NAME, value="entry")[index]
+        cell = row.find_elements(by=By.TAG_NAME, value="td")[index_edit_button]
+        cell.find_element(by=By.TAG_NAME, value="a").click()
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        self.app.open_home_page()
+        row = wd.find_elements(by=By.NAME, value="entry")[index]
+        cell = row.find_elements(by=By.TAG_NAME, value="td")[index_view_button]
+        cell.find_element(by=By.TAG_NAME, value="a").click()
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.open_contact_to_edit_by_index(index)
+        firstname = wd.find_element(by=By.NAME, value="firstname").get_attribute("value")
+        lastname = wd.find_element(by=By.NAME, value="lastname").get_attribute("value")
+        id = wd.find_element(by=By.NAME, value="id").get_attribute("value")
+        homephone = wd.find_element(by=By.NAME, value="home").get_attribute("value")
+        mobilephone = wd.find_element(by=By.NAME, value="mobile").get_attribute("value")
+        workphone = wd.find_element(by=By.NAME, value="work").get_attribute("value")
+        return Contact(firstname=firstname, lastname=lastname, id=id,
+                       homephone=homephone, mobilephone=mobilephone, workphone=workphone)
+
+# структура таблица контактов в HTML-коде главной страницы
+index_id = 0
+index_lastname = 1
+index_firstname = 2
+index_adress = 3
+index_emails = 4
+index_phones = 5
+index_view_button = 6
+index_edit_button = 7
+index_save_contact_button = 8
