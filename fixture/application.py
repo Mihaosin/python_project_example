49bac325edcd30ec85cf3410a224f3590e453bdf
mihaosin <mikhail.osinovskiy@mail.ru>
2022-08-03 21:16:44 +0300

    Задание №16: Реализовать опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения Реализованы опции командной строки, позволяющие указывать тип браузера (chrome, firefox, edge) и адрес тестируемого приложения при запуске тестов.

--- fixture/application.py
+++ fixture/application.py
@@ -6,12 +6,22 @@ from fixture.contact import ContactHelper
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Chrome()
+    def __init__(self, browser, base_url):
+        if browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized broser %s" % browser)
         # self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url=base_url
 
     def is_valid(self):
         try:
@@ -24,7 +34,7 @@ class Application:
         wd = self.wd
         if not wd.current_url.endswith("localhost/addressbook/"):
             # открытие домашней страницы
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
