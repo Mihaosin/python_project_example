c241c5e9a07fbed234daf6d9c8c8eed4d39b7be1
mihaosin <mikhail.osinovskiy@mail.ru>
2022-08-10 15:58:55 +0300

    Задание №19: Реализовать загрузку тестовых данных из файлов Реализована загрузку тестовых данных из файлов в формате JSON и параметризация тестов путём "внедрения" этих тестовых данных в фикстуру при помощи hook-функции pytest_generate_tests.

--- generator/contact.py
+++ generator/contact.py
@@ -1,7 +1,9 @@
 from model.contact import Contact
-from generator.group import random_string
+import random
+import string
 import os.path
 import json
+import jsonpickle
 import getopt
 import sys
 
@@ -24,6 +26,12 @@ for o, a in opts:
         f = a
 
 
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + " "*2
+    # + string.punctuation
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
 testdata = [Contact(firstname="", lastname="")] + [
             Contact(firstname=random_string("firstname"+str(i), 10), lastname=random_string("lastname", 20))
             for i in range(n)
@@ -34,4 +42,5 @@ file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../", f)
 
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -9,8 +9,8 @@ from model.group import Group
 
 
 # тест будет падать, если в имени группы будут присутствовать "'", "\" или более чем один пробел подряд или пробел в конце имени
-def test_add_group(app, data_groups):
-    group = data_groups
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
