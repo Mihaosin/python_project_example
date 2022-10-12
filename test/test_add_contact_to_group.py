f9aae5b85a207df079354344b5cec099d38f68d1
mihaosin <mikhail.osinovskiy@mail.ru>
2022-10-12 21:38:48 +0300

    Задание №22 version 2.0 Модифицирован тест добавления контакта в группу по замечаниям, находим контакт, который не входит хотя бы в одну группу.

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -8,17 +8,32 @@ def test_add_contact_to_group(app, db):
         app.group.create(Group(name="Sasha", footer="Masha"))
     if len(db.get_contact_list()) == 0:
         app.contact.create(Contact(firstname="Pasha"))
-    # блок выбора добавляемого контакта
-    all_contacts = db.get_contact_list()
-    contact = random.choice(all_contacts)
-    # блок выбора модифицируемой группы
-    all_groups = db.get_group_list()
-    group = random.choice(all_groups)
-    # блок получения old_contacts в группе
+    # ищем контакт, который не входит хотя бы в одну группу; запоминаем контакт и группу
+    # если такого контакта нет, создаем новый контакт
+    found = False
+    while not found:
+        all_contacts = db.get_contact_list()
+        all_groups = db.get_group_list()
+        contact_index = 0
+        while (not found) and (contact_index <= len(all_contacts)-1):
+            contact = all_contacts[contact_index]
+            group_index = 0
+            while (not found) and (group_index <= len(all_groups)-1):
+                group = all_groups[group_index]
+                if contact not in db.get_contacts_in_group(group):
+                    found = True
+                group_index += 1
+            contact_index += 1
+        if found == False:
+            contact = Contact(firstname="Pasha")
+            app.contact.create(contact)
+    # конец болка поиска подходящих контакта и группы
+
+    # блок получения old_contacts в выбранной группе
     old_contacts_in_group = db.get_contacts_in_group(group)
     # блок модификации
     app.contact.add_contact_to_group(contact, group)
-    # блок получения new_contacts и редактирования old_contacts в группе
+    # блок получения new_contacts и редактирования old_contacts в выбранной группе
     new_contacts_in_group = db.get_contacts_in_group(group)
     old_contacts_in_group.append(contact)
     # блок проверок
